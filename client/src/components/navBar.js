9bdfdddc06485adbb15c2e216f20acd92cad23f9
morgankn <118085617+morgankn@users.noreply.github.com>
2023-05-01 21:34:52 -0400

    added some components

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -1,74 +1,120 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 // import styled from 'styled-components'
-import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap'
-import { Link} from 'react-router-dom'
-import Auth from '../utils/auth'
+import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+import { Link} from 'react-router-dom';
+// import Auth from '../utils/auth';
 
-function NavBar() {
-    const [showModal, setShowModal] = useState(false);
+// function NavBar({ currentPage, handlePageChange }) {
+//     const [showModal, setShowModal] = useState(false);
+//     return (
+//         <>
+//             <Navbar bg='dark' variant='dark' expand='lg'>
+//                 <Container fluid>
+//                     <Navbar.Brand as={Link} to='/'>
+//                         Google Books Search
+//                     </Navbar.Brand>
+//                     <Navbar.Toggle aria-controls='navbar' />
+//                     <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
+//                         <Nav className='ml-auto d-flex'>
+//                             <Nav.Link as={Link} to='/'>
+//                                 Search For Books
+//                             </Nav.Link>
+//                             {/* if user is logged in show saved books and logout */}
+//                             {/* {Auth.loggedIn() ? ( */}
+//                                 <>
+//                                     <Nav.Link as={Link} to='/saved'>
+//                                         See Your Books
+//                                     </Nav.Link>
+//                                     {/* <Nav.Link onClick={Auth.logout}>Logout</Nav.Link> */}
+//                                 </>
+//                             ) : (
+//                                 <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+//                             )}
+//                         </Nav>
+//                     </Navbar.Collapse>
+//                 </Container>
+//             </Navbar>
+//             {/* set modal data up */}
+//             <Modal
+//                 size='lg'
+//                 show={showModal}
+//                 onHide={() => setShowModal(false)}
+//                 aria-labelledby='signup-modal'>
+//                 {/* tab container to do either signup or login component */}
+//                 <Tab.Container defaultActiveKey='login'>
+//                     <Modal.Header closeButton>
+//                         <Modal.Title id='signup-modal'>
+//                             <Nav variant='pills'>
+//                                 <Nav.Item>
+//                                     <Nav.Link eventKey='login'>Login</Nav.Link>
+//                                 </Nav.Item>
+//                                 <Nav.Item>
+//                                     <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+//                                 </Nav.Item>
+//                             </Nav>
+//                         </Modal.Title>
+//                     </Modal.Header>
+//                     <Modal.Body>
+//                         <Tab.Content>
+//                             <Tab.Pane eventKey='login'>
+//                                 {/* <LoginForm handleModalClose={() => setShowModal(false)} /> */}
+//                             </Tab.Pane>
+//                             <Tab.Pane eventKey='signup'>
+//                                 {/* <SignUpForm handleModalClose={() => setShowModal(false)} /> */}
+//                             </Tab.Pane>
+//                         </Tab.Content>
+//                     </Modal.Body>
+//                 </Tab.Container>
+//             </Modal>
+//         </>
+
+//     )
+// }
+
+//             export default NavBar;
+function NavBar({ currentPage, handlePageChange }) {
     return (
-        <>
-            <Navbar bg='dark' variant='dark' expand='lg'>
-                <Container fluid>
-                    <Navbar.Brand as={Link} to='/'>
-                        Google Books Search
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls='navbar' />
-                    <Navbar.Collapse id='navbar' className='d-flex flex-row-reverse'>
-                        <Nav className='ml-auto d-flex'>
-                            <Nav.Link as={Link} to='/'>
-                                Search For Books
-                            </Nav.Link>
-                            {/* if user is logged in show saved books and logout */}
-                            {Auth.loggedIn() ? (
-                                <>
-                                    <Nav.Link as={Link} to='/saved'>
-                                        See Your Books
-                                    </Nav.Link>
-                                    <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
-                                </>
-                            ) : (
-                                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
-                            )}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            {/* set modal data up */}
-            <Modal
-                size='lg'
-                show={showModal}
-                onHide={() => setShowModal(false)}
-                aria-labelledby='signup-modal'>
-                {/* tab container to do either signup or login component */}
-                <Tab.Container defaultActiveKey='login'>
-                    <Modal.Header closeButton>
-                        <Modal.Title id='signup-modal'>
-                            <Nav variant='pills'>
-                                <Nav.Item>
-                                    <Nav.Link eventKey='login'>Login</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-                                </Nav.Item>
-                            </Nav>
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Tab.Content>
-                            <Tab.Pane eventKey='login'>
-                                {/* <LoginForm handleModalClose={() => setShowModal(false)} /> */}
-                            </Tab.Pane>
-                            <Tab.Pane eventKey='signup'>
-                                {/* <SignUpForm handleModalClose={() => setShowModal(false)} /> */}
-                            </Tab.Pane>
-                        </Tab.Content>
-                    </Modal.Body>
-                </Tab.Container>
-            </Modal>
-        </>
-
-    )
-}
-
-            export default NavBar;
+          <nav className="menu">
+  
+  <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a href='#home'
+            onClick={() => handlePageChange('Home')}
+            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+          >
+            
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#search"
+            onClick={() => handlePageChange('Search')}
+            className={currentPage === 'Search' ? 'nav-link active' : 'nav-link'}
+          >
+            Search
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#profile"
+            onClick={() => handlePageChange('Profile')}
+            className={currentPage === 'Profile' ? 'nav-link active' : 'nav-link'}
+          >
+            Profile
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#friends"
+            onClick={() => handlePageChange('Friends')}
+            className={currentPage === 'Friends' ? 'nav-link active' : 'nav-link'}
+          >
+            Friends?
+          </a>
+        </li>
+      </ul>
+      </nav>
+    );
+  }
+  
+  export default NavBar;
---
+++ client/src/pages/Profile.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Profile(){
+    return (
+        <p>This is where the profile will be</p>
+    )
+}
+
+export default Profile;
