cabcca36e69c1478c101d32550e0dee21eb5138a
morgankn <118085617+morgankn@users.noreply.github.com>
2023-05-03 21:46:50 -0400

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,48 @@
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import React from "react";
-import Home from "./pages/Home";
-import Footer from "./components/Footer";
+import Profile from './pages/Profile';
+import Footer from './components/Footer';
+import Search from './pages/Search';
+import NavBar from './components/navBar';
+import Landing from './pages/Landing';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <NavBar />
+          <div className="container">
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Landing />} 
+              />
+               <Route 
+                path="/search" 
+                element={<Search />} 
+              />
+              <Route 
+                path="/profiles/:profileId" 
+                element={<Profile />} 
+              />
+               <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
