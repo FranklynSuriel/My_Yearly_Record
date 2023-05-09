cf25b55d0612a1ceb9ec017cb42880ea891b55ef
Luna Rae <kra7888@gmail.com>
2023-05-08 20:36:17 -0400

    working movie search

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,22 +2,29 @@
 
 const API_KEY = process.env.API_KEY;
 
-export const searchTMDB = (query) => {
-	console.log(API_KEY)
-	return fetch(
-		`https://api.themoviedb.org/3/search/tv?api_key=32ce25589aa56c85a8438a669253213c&query=${query}&include_adult=false`
-	)
-	.then(response => response.json())
-    .then(data => {
-      console.log(data);
-      return data;
-    })
-    .catch(error => {
-      console.error(error);
-      throw new Error('Search to TMDB API failed')
-    });
-};
+// export const searchTMDB = (query) => {
+// 	console.log(API_KEY)
+// 	return fetch(
+// 		`https://api.themoviedb.org/3/search/tv?api_key=32ce25589aa56c85a8438a669253213c&query=${query}&include_adult=false`
+// 	)
+// 	.then(response => response.json())
+//     .then(data => {
+//       console.log(data);
+//       return data;
+//     })
+//     .catch(error => {
+//       console.error(error);
+//       throw new Error('Search to TMDB API failed')
+//     });
+// };
 
+export const searchTMDB = async (query) => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/tv?api_key=32ce25589aa56c85a8438a669253213c&query&language=en-US&query=${query}&page=1`
+  );
+  const data = await response.json(); // Parse response as JSON
+  return data.results;
+};
 // export const searchTMDB = async (query) => {
 // 	// const apiKeyResponse = await fetch('/api/get-key');
 // 	// const {apiKey} = await apiKeyResponse.json();
